08d8b52e4ad135cca654664110934b93a8fbe577
Md. Musfiqur Rahman <mrifat46@gmail.com>
2026-04-13 23:23:56 +0600

    addded db

--- cms_backend/core/settings.py
+++ cms_backend/core/settings.py
@@ -59,17 +59,10 @@ ASGI_APPLICATION = "core.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.getenv("DB_NAME", BASE_DIR / "db_runtime.sqlite3"),
-        "USER": os.getenv("DB_USER", ""),
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
-        "HOST": os.getenv("DB_HOST", ""),
-        "PORT": os.getenv("DB_PORT", ""),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-if DATABASES["default"]["ENGINE"] == "django.db.backends.sqlite3":
-    DATABASES["default"]["NAME"] = str(DATABASES["default"]["NAME"])
 
 AUTH_PASSWORD_VALIDATORS = [
     {
